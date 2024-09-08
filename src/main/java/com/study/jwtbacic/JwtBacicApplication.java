71e90c2f79c7b2a6b7b5d68a3a3933e87473c5f3
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-08 12:08:54 -0300

    Primeiro commit

---
+++ src/main/java/com/study/jwtbacic/JwtBacicApplication.java
@@ -0,0 +1,13 @@
+package com.study.jwtbacic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBacicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtBacicApplication.class, args);
+    }
+
+}
