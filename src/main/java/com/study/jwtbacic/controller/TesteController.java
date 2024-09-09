fe4f45edd7c59819b0d872de4cb0b9871761d3fc
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-09 00:19:37 -0300

    - Criada a controller SecurityConfiguration

---
+++ src/main/java/com/study/jwtbacic/controller/TesteController.java
@@ -0,0 +1,27 @@
+package com.study.jwtbacic.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/test")
+public class TesteController {
+
+    @GetMapping("/liberado")
+    public ResponseEntity<String> endpointLiberado(){
+        return new ResponseEntity<>("Liberado", HttpStatus.OK);
+    }
+
+    @GetMapping("/requer_autenticacao")
+    public ResponseEntity<String> requerAutenticacao(){
+        return new ResponseEntity<>("Autenticado", HttpStatus.OK);
+    }
+
+    @GetMapping("/requer_autenticacao_admin")
+    public ResponseEntity<String> requerAutenticacaoAdmin(){
+        return new ResponseEntity<>("Autenticado admin", HttpStatus.OK);
+    }
+}
