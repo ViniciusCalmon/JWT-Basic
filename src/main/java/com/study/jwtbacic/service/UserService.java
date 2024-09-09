64bde16380ea010292b30b513867329602ce7583
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-09 00:21:12 -0300

    - Criada a service UserService

---
+++ src/main/java/com/study/jwtbacic/service/UserService.java
@@ -0,0 +1,21 @@
+package com.study.jwtbacic.service;
+
+import com.study.jwtbacic.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
