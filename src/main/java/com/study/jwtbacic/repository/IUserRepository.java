ef5446cfd65085cdb6f4b3459546b25c98e8b26f
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-09 00:16:51 -0300

    - Criado criado os repositories IRoles e IUser

--- src/main/java/com/study/jwtbacic/repository/IUserRepository.java
+++ src/main/java/com/study/jwtbacic/repository/IUserRepository.java
@@ -3,5 +3,9 @@ package com.study.jwtbacic.repository;
 import com.study.jwtbacic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
