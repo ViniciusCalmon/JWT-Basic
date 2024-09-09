46e478d815722fbd024ec73e069e570ec4ddfc39
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-09 13:33:22 -0300

    - Adicionado um endpoint de test para checar as roles dos tokens

--- src/main/java/com/study/jwtbacic/controller/TesteController.java
+++ src/main/java/com/study/jwtbacic/controller/TesteController.java
@@ -2,6 +2,7 @@ package com.study.jwtbacic.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +25,10 @@ public class TesteController {
     public ResponseEntity<String> requerAutenticacaoAdmin(){
         return new ResponseEntity<>("Autenticado admin", HttpStatus.OK);
     }
+
+    @GetMapping("/roles")
+    public ResponseEntity<?> getUserRoles(Authentication authentication) {
+        return ResponseEntity.ok(authentication.getAuthorities());
+    }
+
 }
