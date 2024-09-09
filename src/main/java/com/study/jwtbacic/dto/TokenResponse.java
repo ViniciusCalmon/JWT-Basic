f51e26599bef0909e64fad27651904b0fac87393
ViniciusCalmon <viniciuscalmon1@gmail.com>
2024-09-09 00:20:19 -0300

    - Criada a dto TokenResponse

---
+++ src/main/java/com/study/jwtbacic/dto/TokenResponse.java
@@ -0,0 +1,19 @@
+package com.study.jwtbacic.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenResponse {
+
+    private String token;
+
+    private Long expiresIn;
+
+    private String username;
+}
